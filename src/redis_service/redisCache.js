4c9628093836ab6b4501b5921f046c68ad6e0d1b
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2023-04-09 21:59:55 -0300

    atualizando funcao para aceitar 2000 tokens

--- src/redis_service/redisCache.js
+++ src/redis_service/redisCache.js
@@ -16,26 +16,20 @@ export async function saveMessageToCache({ userId, role, content }, client) {
   try {
     const getAsync = promisify(client.get).bind(client);
     const setAsync = promisify(client.set).bind(client);
-    const userConversationKey = `user:${userId}:conversation`;
-    const userTokenCountKey = `user:${userId}:tokenCount`;
-    const message = { role, content };
-    const messageTokens = content.split(' ').length;
+    const conversationStats = await getAsync('conversationStats');
+    const conversation = JSON.parse(conversationStats) || {};
+    const userMessages = conversation[userId] || [];
 
-    // Obter o número atual de tokens
-    let tokenCount = parseInt(await getAsync(userTokenCountKey), 10) || 0;
+    userMessages.push({ role, content });
 
-    // Se o limite de tokens for excedido, remover mensagens mais antigas até que o limite seja respeitado
-    while (tokenCount + messageTokens > 2000) {
-      const removedMessage = JSON.parse(await client.lpop(userConversationKey));
-      const removedMessageTokens = removedMessage.content.split(' ').length;
-      tokenCount -= removedMessageTokens;
+    if (countTokens(userMessages) > 2000) {
+      userMessages.shift();
     }
 
-    // Adicionar a nova mensagem e atualizar o número de tokens
-    await client.rpush(userConversationKey, JSON.stringify(message));
-    await setAsync(userTokenCountKey, tokenCount + messageTokens);
-
+    conversation[userId] = userMessages;
+    await setAsync('conversationStats', JSON.stringify(conversation), 'EX', 3600);
   } catch (error) {
     throw error;
   }
 }
+
